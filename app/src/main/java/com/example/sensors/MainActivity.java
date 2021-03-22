2e0bbd911b5aa0ffb6575898e057fe176b1cc4f8
official-akhil <akhil20107@iiitd.ac.in>
2021-03-23 01:48:14 +0530

    All done

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.sensors;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -13,11 +11,20 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
-import android.provider.Settings;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -28,7 +35,111 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+@Database(entities = {Accelerometer_data.class, Light.class}, version = 1)
+abstract class AppDatabase extends RoomDatabase {
+    public abstract SensorDao senserDao();
+}
+
+@Entity
+class Accelerometer_data {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "x-axis")
+    public float x;
+
+    @ColumnInfo(name = "y-axis")
+    public float y;
+
+    @ColumnInfo(name = "z-axis")
+    public float z;
+
+    @ColumnInfo(name = "date")
+    public String d;
+
+    public float getX()
+    {
+        return x;
+    }
+    public void setX(float x_val)
+    {
+        this.x = x_val;
+    }
+    public float getY()
+    {
+        return y;
+    }
+    public void setY(float y_val)
+    {
+        this.y = y_val;
+    }
+    public float getZ()
+    {
+        return z;
+    }
+    public void setZ(float z_val)
+    {
+        this.z = z_val;
+    }
+    public String getd()
+    {
+        return d;
+    }
+    public void setd(String da)
+    {
+        this.d = da;
+    }
+}
+
+@Entity
+class Light{
+    @PrimaryKey(autoGenerate = true)
+    public int lid;
+
+    @ColumnInfo(name = "ilum")
+    public float ilum;
+
+    @ColumnInfo(name = "ldate")
+    public String light_date;
+
+    public float getilum()
+    {
+        return ilum;
+    }
+    public void setilum(float ilum_val)
+    {
+        this.ilum = ilum_val;
+    }
+    public String getLight_date()
+    {
+        return light_date;
+    }
+    public void setLight_date(String ld)
+    {
+        this.light_date = ld;
+    }
+
+}
+
+@Dao
+interface SensorDao {
+    @Insert
+   void addAcc(Accelerometer_data value);
+    @Query("select * from Accelerometer_data WHERE date >= DATE('now', '-1 hour')")
+    public List<Accelerometer_data> getaccValues();
+
+    @Insert
+    void addLight(Light l_val);
+    @Query("select * from Light WHERE ldate >= DATE('now', '-1 hour')")
+    public List<Light> getlightValues();
+}
+
 
 public class MainActivity extends AppCompatActivity {
     private SensorManager mSensorManager;
@@ -36,10 +147,22 @@ public class MainActivity extends AppCompatActivity {
     private LocationManager locationManager;
     private LocationListener listener;
     private Sensor mAcc, lAcc, light, prox, temp;
+    private float[] mGravity;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    private int hitCount = 0;
+    private double hitSum = 0;
+    private double hitResult = 0;
+
+    private final int SAMPLE_SIZE = 50; // change this sample size as you want, higher is more precise but slow measure.
+    private final double THRESHOLD = 0.3;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch acc_switch,lacc_switch, light_switch, prox_switch, temp_switch, loc_switch;
-    TextView acc_x, acc_y, acc_z, l_acc_x, l_acc_y, l_acc_z, light_val, prox_value, temp_value, lati, longi;
+    TextView acc_x, acc_y, acc_z, l_acc_x, l_acc_y, l_acc_z, light_val, prox_value, temp_value, lati, longi, show_acc,show_light, check_walk;
     int flag,flag1,flag2,flag3,flag4,flag5=0;
+    float total_x,total_y,total_z,avg_x,avg_y,avg_z, total_l, avg_l;
+    Button b,l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +174,11 @@ public class MainActivity extends AppCompatActivity {
         prox = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         temp = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+        b=findViewById(R.id.button);
+        l = findViewById(R.id.light_button);
         temp_value = findViewById(R.id.temperature);
         if (temp == null) {
             temp_value.setText("This sensor is not available");
@@ -67,6 +195,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "Sensorsdata").allowMainThreadQueries().build();
+        SensorDao senserDao = db.senserDao();
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show_acc = findViewById(R.id.textView3);
+                total_x = 0;
+                total_y = 0;
+                total_z = 0;
+                avg_x = 0;
+                avg_y = 0;
+                avg_z = 0;
+
+                List<Accelerometer_data> acc_values = senserDao.getaccValues();
+//                Toast.makeText(MainActivity.this,"Total values recorded : "+ acc_values.size(), Toast.LENGTH_SHORT).show();
+                for (Accelerometer_data accel : acc_values) {
+                        float x = accel.getX();
+                        float y = accel.getY();
+                        float z = accel.getZ();
+                        total_x = total_x + x;
+                        total_y = total_y + y;
+                        total_z = total_z + z;
+//                    }
+                    avg_x = total_x / acc_values.size();
+                    avg_y = total_y / acc_values.size();
+                    avg_z = total_z / acc_values.size();
+                    String avg = "Total values recorded : " + acc_values.size() + "\nAverage X = " + avg_x + "\nAverage Y = " + avg_y + "\nAverage Z = " + avg_z + "\n\n";
+                    show_acc.setText(avg);
+                    show_acc.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show_light = findViewById(R.id.textView4);
+                total_l=0;
+                avg_l=0;
+                List<Light> light_values = senserDao.getlightValues();
+//                Toast.makeText(MainActivity.this,"Total values recorded : "+ light_values.size(), Toast.LENGTH_SHORT).show();
+                    for(Light lig:light_values)
+                {
+                    float x = lig.getilum();
+                    total_l = total_l + x;
+                }
+                avg_l = total_l/light_values.size();
+                String avg_li = "Total values recorded : " + light_values.size() + "\nAverage Light = " + avg_l + "\n\n";
+                show_light.setText(avg_li);
+                show_light.setVisibility(View.VISIBLE);
+//                    Toast.makeText(getApplicationContext(),"Average Illuminance = "+avg_l, Toast.LENGTH_SHORT).show();
+            }
+        });
         listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
@@ -220,18 +402,58 @@ public class MainActivity extends AppCompatActivity {
 
                 if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
                     if(flag==0) {
+                        check_walk = findViewById(R.id.textView5);
                         acc_x = findViewById(R.id.xaxis);
                         acc_y = findViewById(R.id.yaxis);
                         acc_z = findViewById(R.id.zaxis);
                         float x = event.values[0];
                         float y = event.values[1];
                         float z = event.values[2];
+                        mAccelLast = mAccelCurrent;
+                        mAccelCurrent = (float)Math.sqrt(x*x + y*y + z*z);
+                        float delta = mAccelCurrent - mAccelLast;
+                        mAccel = mAccel * 0.9f + delta;
+                        if (hitCount <= SAMPLE_SIZE) {
+                            hitCount++;
+                            hitSum += Math.abs(mAccel);
+                        } else {
+                            hitResult = hitSum / SAMPLE_SIZE;
+
+                            Log.d("dist", String.valueOf(hitResult));
+
+                            if (hitResult > THRESHOLD) {
+                                check_walk.setText("Walking");
+                            } else {
+                                check_walk.setText("Stationary");
+                            }
+
+                            hitCount = 0;
+                            hitSum = 0;
+                            hitResult = 0;
+                        }
+//                        if(mAccel > 1){
+//
+//                            check_walk.setText("Walking");
+//                        }
+//                        else
+//                        {
+//                            check_walk.setText("Stationary");
+//                        }
+                        Date acc_cal = Calendar.getInstance().getTime();
+                        SimpleDateFormat acc_date = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss", Locale.getDefault());
+                        String requiredDateformat = acc_date.format(acc_cal);
                         String acc_x_data = "X=" + x;
                         String acc_y_data = "Y=" + y;
                         String acc_z_data = "Z=" + z;
                         acc_x.setText(acc_x_data);
                         acc_y.setText(acc_y_data);
                         acc_z.setText(acc_z_data);
+                        Accelerometer_data acc_val = new Accelerometer_data();
+                        acc_val.setX(x);
+                        acc_val.setY(y);
+                        acc_val.setZ(z);
+                        acc_val.setd(requiredDateformat);
+                        senserDao.addAcc(acc_val);
                     }
                     else
                     {
@@ -266,10 +488,17 @@ public class MainActivity extends AppCompatActivity {
                     }
                 } else if (sensor.getType() == Sensor.TYPE_LIGHT) {
                     if(flag2==0) {
+                        Date cal = Calendar.getInstance().getTime();
+                        SimpleDateFormat l_date = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss", Locale.getDefault());
+                        String l_requiredDateformat = l_date.format(cal);
                         float x = event.values[0];
                         light_val = findViewById(R.id.light);
                         String l = "Illuminance = " + x;
                         light_val.setText(l);
+                        Light light_val = new Light();
+                        light_val.setilum(x);
+                        light_val.setLight_date(l_requiredDateformat);
+                        senserDao.addLight(light_val);
                     }
                     else
                     {
